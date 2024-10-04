ba1a1c5b367977353a7755b13df7a065ba4fbea0
Buba <buba.bbojang@gmail.com>
2024-10-04 00:43:48 +0000

    Revert "feat: App initialized. Routes, models, controllers and migrations created. Authentication with access token working without the refresh token." Forgot to create a branch. This reverts commit 078bcf0700b37ddbd52b5b3e0407118d2e461620.

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Http\Controllers\ReminderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SessionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,27 +16,4 @@ use App\Http\Controllers\SessionController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
-
-Route::post('/session', [SessionController::class, 'login'])
-                ->middleware('guest')
-                ->name('login');
-
-Route::put('/session', [SessionController::class, 'refreshToken'])
-                ->middleware('guest')
-                ->name('refresh-token');
-
-Route::post('/logout', [SessionController::class, 'logout'])
-                ->middleware('guest')
-                ->name('logout');
-                
-
-Route::middleware(['auth:sanctum'])->group(function () {
-    Route::controller(ReminderController::class)->group(function () {
-        Route::get('/reminders', 'index')->name('reminders.index');
-        Route::get('/reminders/{id}', 'show')->name('reminders.show');
-        Route::post('/reminders', 'store')->name('reminders.store');
-        Route::put('/reminders/{id}', 'update')->name('reminders.update');
-        Route::delete('/reminders/{id}', 'destroy')->name('reminders.delete');
-    });
 });
